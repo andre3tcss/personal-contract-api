c7b24054ec03ba8d7ed62565c94c49106e427e91
André <andregasparino12@gmail.com>
2025-09-11 19:53:02 -0300

    feat: initial project structure from Spring Initializr

---
+++ src/main/java/br/com/andre/personalcontractapi/PersonalContractApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.andre.personalcontractapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalContractApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalContractApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/andre/personalcontractapi/PersonalContractApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andre.personalcontractapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalContractApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
