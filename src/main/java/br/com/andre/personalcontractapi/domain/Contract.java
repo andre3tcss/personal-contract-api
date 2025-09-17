7a9699430d029507b88dded2b43d4d5edf56cf12
André <andregasparino12@gmail.com>
2025-09-17 14:13:43 -0300

    feat: add Contract entity and ContractRepository

---
+++ src/main/java/br/com/andre/personalcontractapi/domain/Contract.java
@@ -0,0 +1,35 @@
+package br.com.andre.personalcontractapi.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+
+@Entity
+
+@Table(name = "tb_contract")
+
+@Data
+
+@NoArgsConstructor
+
+@AllArgsConstructor
+public class Contract {
+
+    @Id
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String clientName;
+
+    @Column(length = 1000)
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal value;
+
+}
+
